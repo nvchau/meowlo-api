17287baf9c43062b10a174a9bda4795325942e98
Nguyễn Văn Châu <90164150+st-chaunguyen2@users.noreply.github.com>
2022-04-26 10:57:03 +0700

    Combine API full board

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -14,6 +14,7 @@ const createNew = async ({ data }) => {
 const getFullBoard = async ({ id }) => {
   try {
     const board = await BoardModel.getFullBoard({ id })
+    if (!board || !board.columns) throw new Error('Board not found')
 
     // Add card to each column
     board.columns.forEach(column => {
